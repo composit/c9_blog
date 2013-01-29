2b7c9a44000bfe99936077c8fea185dd7b7cbdce
Mr. Matt <matt@composition9.com>
2013-01-28 18:11:35 -0700

    ruby version in deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :stages, %w( staging production )
 set :default_stage, 'staging'
 
 require 'rvm/capistrano'
-set :rvm_ruby_string, '1.9.2-p290'
+set :rvm_ruby_string, '1.9.3-p374'
 set :rvm_type, :system
 
 require 'bundler/capistrano'
